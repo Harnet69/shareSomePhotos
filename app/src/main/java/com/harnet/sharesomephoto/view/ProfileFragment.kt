eec7a4cce044892e19dcd4f47087e66af46fb52e
Harnet69 <harnet69@gmail.com>
2020-11-21 19:26:14 +0100

    Added details to profile

--- app/src/main/java/com/harnet/sharesomephoto/view/ProfileFragment.kt
+++ app/src/main/java/com/harnet/sharesomephoto/view/ProfileFragment.kt
@@ -5,21 +5,20 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
-import android.widget.LinearLayout
-import android.widget.Toast
+import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.harnet.lookatthis.model.User
+import com.harnet.lookatthis.model.UserParsable
 import com.harnet.sharesomephoto.R
 import com.harnet.sharesomephoto.databinding.ProfileFragmentBinding
 import com.harnet.sharesomephoto.viewModel.ProfileViewModel
+import com.parse.ParseUser
 
-class ProfileFragment : Fragment() {
+class ProfileFragment : Fragment(), UserParsable {
     private lateinit var viewModel: ProfileViewModel
     private lateinit var dataBinding: ProfileFragmentBinding
 
@@ -31,6 +30,11 @@ class ProfileFragment : Fragment() {
     lateinit var userEmailField: EditText
     lateinit var signUpBtn: Button
     lateinit var logInBtn: Button
+
+    lateinit var userNameTextView: TextView
+    lateinit var userEmailTextView: TextView
+
+    var currentUser: ParseUser? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -53,11 +57,14 @@ class ProfileFragment : Fragment() {
         signUpBtn = view.findViewById(R.id.sign_up_btn)
         logInBtn = view.findViewById(R.id.login_btn)
 
+        userNameTextView = view.findViewById(R.id.userName_TextView)
+        userEmailTextView = view.findViewById(R.id.userEmail_TextView)
+
         viewModel = ViewModelProvider(this).get(ProfileViewModel::class.java)
 
         // if user have been logged already
-        val isLogged = viewModel.isLogged()
-        if (isLogged == null) {
+        currentUser = viewModel.isLogged()
+        if (currentUser == null) {
             signUpBtn.setOnClickListener {
                 viewModel.addNewUser(
                     User(
@@ -74,11 +81,11 @@ class ProfileFragment : Fragment() {
             observeModel()
         } else {
             userLoginBlock.setVisibility(View.GONE)
-            Toast.makeText(context, "Hello ${isLogged.username}", Toast.LENGTH_LONG).show()
+            Toast.makeText(context, "Hello ${currentUser?.username}", Toast.LENGTH_LONG).show()
             userProfileDetailsBlock.setVisibility(View.VISIBLE)
-
             //TODO profile details block
-
+            userNameTextView.text = currentUser?.username
+            userEmailTextView.text = currentUser?.email
         }
     }
 
@@ -96,7 +103,9 @@ class ProfileFragment : Fragment() {
                 userLoginBlock.setVisibility(View.GONE)
                 userProfileDetailsBlock.setVisibility(View.VISIBLE)
                 //TODO switch on the profile details block
-                Toast.makeText(context, "Hello", Toast.LENGTH_LONG).show()
+                Toast.makeText(context, "Hello " + isLogged()?.username, Toast.LENGTH_LONG).show()
+                userNameTextView.text = isLogged()?.username
+                userEmailTextView.text = isLogged()?.email
             }
         })
     }
