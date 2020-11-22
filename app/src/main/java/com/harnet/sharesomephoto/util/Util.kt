6639f9d39daecb8b40b1dfece6879603056a1290
Harnet69 <harnet69@gmail.com>
2020-11-22 18:47:17 +0100

    Redirecting not logged users to Profile

--- app/src/main/java/com/harnet/sharesomephoto/util/Util.kt
+++ app/src/main/java/com/harnet/sharesomephoto/util/Util.kt
@@ -1,1 +1,14 @@
 package com.harnet.sharesomephoto.util
+
+import android.util.Log
+import com.parse.ParseUser
+
+//check if the user logged in
+fun isLogged(): ParseUser? {
+    if(ParseUser.getCurrentUser() != null){
+        Log.i("tweet", "isLogged: you logged as: ${ParseUser.getCurrentUser().username}")
+    }else{
+        Log.i("tweet", "isLogged: ${ParseUser.getCurrentUser()}")
+    }
+    return ParseUser.getCurrentUser()
+}
--- app/src/main/java/com/harnet/sharesomephoto/view/FeedsFragment.kt
+++ app/src/main/java/com/harnet/sharesomephoto/view/FeedsFragment.kt
@@ -2,6 +2,7 @@ package com.harnet.sharesomephoto.view
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -9,6 +10,8 @@ import androidx.navigation.Navigation
 import com.harnet.sharesomephoto.viewModel.FeedsViewModel
 import com.harnet.sharesomephoto.R
 import com.harnet.sharesomephoto.databinding.ProfileFragmentBinding
+import com.harnet.sharesomephoto.util.isLogged
+import com.parse.ParseUser
 
 class FeedsFragment : Fragment() {
 
@@ -23,6 +26,11 @@ class FeedsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // redirect to Profile if a user not logged
+        if(ParseUser.getCurrentUser() == null){
+            goToProfile()
+        }
         // switch on a menu
         setHasOptionsMenu(true)
         viewModel = ViewModelProvider(this).get(FeedsViewModel::class.java)
@@ -40,10 +48,7 @@ class FeedsFragment : Fragment() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.profile -> {
-                view?.let {
-                    Navigation.findNavController(it)
-                        .navigate(FeedsFragmentDirections.actionFeedsFragmentToProfileFragment())
-                }
+                goToProfile()
             }
         }
         return super.onOptionsItemSelected(item)
@@ -51,5 +56,12 @@ class FeedsFragment : Fragment() {
 
     private fun observeViewModel() {
 
+    }
+
+    private fun goToProfile(){
+        view?.let {
+            Navigation.findNavController(it)
+                .navigate(FeedsFragmentDirections.actionFeedsFragmentToProfileFragment())
+        }
     }
 }
