3a2179629c8a828b83a5746a411f3cdd2f24a258
Harnet69 <harnet69@gmail.com>
2020-11-26 19:55:24 +0100

    Implemented swiper for feeds refreshing

--- app/src/main/java/com/harnet/sharesomephoto/view/FeedsFragment.kt
+++ app/src/main/java/com/harnet/sharesomephoto/view/FeedsFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import com.harnet.sharesomephoto.R
@@ -12,6 +13,7 @@ import com.harnet.sharesomephoto.util.openImageChooser
 import com.harnet.sharesomephoto.viewModel.FeedsViewModel
 import com.parse.ParseUser
 import kotlinx.android.synthetic.main.feeds_fragment.*
+import kotlinx.android.synthetic.main.users_fragment.*
 
 class FeedsFragment : Fragment() {
 
@@ -28,15 +30,16 @@ class FeedsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         viewModel = ViewModelProvider(this).get(FeedsViewModel::class.java)
-
+        Log.i("Availableimages", "onViewCreated: ")
         viewModel.refresh()
 
-        val userImages = viewModel.mImages.value
-        if( userImages != null){
-            for(image in userImages){
-                Log.i("Availableimages", "${image.url}  :  ${image.userName}")
-            }
-        }
+
+//        val userImages = viewModel.mImages.value
+//        if( userImages != null){
+//            for(image in userImages){
+//                Log.i("Availableimages", "${image.url}  :  ${image.userName}")
+//            }
+//        }
 
         // redirect to Profile if a user not logged
         if(ParseUser.getCurrentUser() == null){
@@ -49,6 +52,16 @@ class FeedsFragment : Fragment() {
         // Add/change user image
         addImage_btn.setOnClickListener {
             (activity as MainActivity).appPermissions.imagesService.checkPermission()
+        }
+
+        // Swiper refresh listener(screen refreshing process)
+        refreshLayout_feedsFragment.setOnRefreshListener {
+//            feeds_list_feedsFragment.visibility = View.GONE
+            listError_TextView_feedsFragment.visibility = View.GONE
+            loadingView_ProgressBar_feedsFragment.visibility = View.VISIBLE
+            viewModel.refresh()
+            refreshLayout_feedsFragment.isRefreshing = false // disappears little spinner on the top
+
         }
 
         observeViewModel()
@@ -74,7 +87,45 @@ class FeedsFragment : Fragment() {
     }
 
     private fun observeViewModel() {
+        // update the layout using values of mutable variables from a ViewModel
+        viewModel.mImages.observe(viewLifecycleOwner, Observer { images ->
+            images?.let {
+//                users_list_usersFragment.visibility = View.VISIBLE
+//                usersAdapter.updateUsersList(images)
+            }
+        })
 
+        // make error TextViewVisible
+        viewModel.mIsImageLoadError.observe(viewLifecycleOwner, Observer { isError ->
+            // check isError not null
+            isError?.let {
+                listError_TextView_feedsFragment.visibility = if (it) View.VISIBLE else View.GONE
+            }
+        })
+
+        // loading spinner
+        viewModel.mIsLoading.observe(viewLifecycleOwner, Observer { isLoading ->
+            //check isLoading not null
+            isLoading?.let {
+                //TODO show all photos from here
+                // for test purposes
+                val userImages = viewModel.mImages.value
+                if( userImages != null){
+                    for(image in userImages){
+                        Log.i("Availableimages", "${image.url}  :  ${image.userName}")
+                    }
+                }
+
+                // if data still loading - show spinner, else - remove it
+                loadingView_ProgressBar_feedsFragment.visibility =
+                    if (it) View.VISIBLE else View.GONE
+                if (it) {
+                    //hide all views when progress bar is visible
+                    listError_TextView_feedsFragment.visibility = View.GONE
+//                    users_list_usersFragment.visibility = View.GONE
+                }
+            }
+        })
     }
 
     // redirect to the Profile page
