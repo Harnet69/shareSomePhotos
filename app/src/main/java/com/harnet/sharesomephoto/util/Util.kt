2ff00cd012569446dd5cfc95ce6420b3d9555ff7
Harnet69 <harnet69@gmail.com>
2020-11-27 19:11:52 +0100

    Implemented redirecting to Image page

--- app/src/main/java/com/harnet/sharesomephoto/util/Util.kt
+++ app/src/main/java/com/harnet/sharesomephoto/util/Util.kt
@@ -4,11 +4,14 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.graphics.drawable.Drawable
+import android.util.Log
+import android.view.View
 import android.widget.ImageView
 import androidx.annotation.StringRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.BindingAdapter
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
@@ -18,8 +21,10 @@ import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
 import com.harnet.sharesomephoto.R
+import com.harnet.sharesomephoto.service.OnSingleClickListenerService
+import com.harnet.sharesomephoto.view.FeedsFragmentDirections
 
-fun openImageChooser(activity: Activity){
+fun openImageChooser(activity: Activity) {
     val intent = Intent()
     intent.type = "image/*"
     intent.action = Intent.ACTION_GET_CONTENT
@@ -72,11 +77,28 @@ fun loadBindingImage(view: ImageView, url: String?) {
 }
 
 //extension for a user title changing
-fun Fragment.setActivityTitle(@StringRes id: Int)
-{
+fun Fragment.setActivityTitle(@StringRes id: Int) {
     (activity as AppCompatActivity?)!!.supportActionBar?.title = getString(id)
 }
-fun Fragment.setActivityTitle(title: String)
-{
+
+fun Fragment.setActivityTitle(title: String) {
     (activity as AppCompatActivity?)!!.supportActionBar?.title = title
+}
+
+// go to Image page from feeds
+@BindingAdapter("android:goToImagePage")
+fun goToImagePage(view: ImageView, imageUrl: String?) {
+    // prevent a crash when two items were clicked in the same time
+    fun View.setOnSingleClickListener(l: (View) -> Unit) {
+        setOnClickListener(OnSingleClickListenerService(l))
+    }
+
+    view.setOnSingleClickListener {imageView ->
+        // navigate to appropriate detail fragment
+        imageUrl?.let {
+            val action = FeedsFragmentDirections.actionFeedsFragmentToImageFragment(imageUrl)
+            // send image url to Image fragment
+            Navigation.findNavController(imageView).navigate(action)
+        }
+    }
 }
