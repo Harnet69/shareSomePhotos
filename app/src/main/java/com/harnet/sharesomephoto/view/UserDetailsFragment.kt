e480e1e885fe6c2b1ae5937d59eee7a99c5f38ab
Harnet69 <harnet69@gmail.com>
2020-11-28 21:42:43 +0100

    Implemented viewModel for UserDetails screen

--- app/src/main/java/com/harnet/sharesomephoto/view/UserDetailsFragment.kt
+++ app/src/main/java/com/harnet/sharesomephoto/view/UserDetailsFragment.kt
@@ -7,9 +7,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import com.harnet.sharesomephoto.R
 import com.harnet.sharesomephoto.databinding.UserDetailsFragmentBinding
 import com.harnet.sharesomephoto.viewModel.UserDetailsViewModel
+import kotlinx.android.synthetic.main.user_details_fragment.*
+import kotlinx.android.synthetic.main.users_fragment.*
 
 class UserDetailsFragment : Fragment() {
     private lateinit var viewModel: UserDetailsViewModel
@@ -22,11 +25,44 @@ class UserDetailsFragment : Fragment() {
         dataBinding = DataBindingUtil.inflate(inflater, R.layout.user_details_fragment, container, false)
 
         return dataBinding.root
-//        return inflater.inflate(R.layout.user_details_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this).get(UserDetailsViewModel::class.java)
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        // update the layout using values of mutable variables from a ViewModel
+        viewModel.mUser.observe(viewLifecycleOwner, Observer { user ->
+            user?.let {
+                user_detail_block_userDeskrFragment.visibility = View.VISIBLE
+            }
+        })
+
+        // make error TextViewVisible
+        viewModel.mIsUserLoadError.observe(viewLifecycleOwner, Observer { isError ->
+            // check isError not null
+            isError?.let {
+                listError_TextView_UserDescr.visibility = if (it) View.VISIBLE else View.GONE
+            }
+        })
+
+        // loading spinner
+        viewModel.mIsLoading.observe(viewLifecycleOwner, Observer { isLoading ->
+            //check isLoading not null
+            isLoading?.let {
+                // if data still loading - show spinner, else - remove it
+                loadingView_ProgressBar_UserDescr.visibility =
+                    if (it) View.VISIBLE else View.GONE
+                if (it) {
+                    //hide all views when progress bar is visible
+                    listError_TextView_UserDescr.visibility = View.GONE
+                    user_detail_block_userDeskrFragment.visibility = View.GONE
+                }
+            }
+        })
     }
 }
--- app/src/main/java/com/harnet/sharesomephoto/viewModel/UserDetailsViewModel.kt
+++ app/src/main/java/com/harnet/sharesomephoto/viewModel/UserDetailsViewModel.kt
@@ -1,7 +1,55 @@
 package com.harnet.sharesomephoto.viewModel
 
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import com.harnet.sharesomephoto.model.User
+import com.parse.FindCallback
+import com.parse.ParseQuery
+import com.parse.ParseUser
 
-class UserDetailsViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class UserDetailsViewModel(application: Application) : BaseViewModel(application) {
+    val mUser = MutableLiveData<User>()
+    val mIsUserLoadError = MutableLiveData<Boolean>()
+    val mIsLoading = MutableLiveData<Boolean>()
+
+    fun refresh(username: String) {
+        getUserFromParseServer(username)
+    }
+
+    // retrieve images
+    private fun retrieveUser(userFromParse: User) {
+        // set received list to observable mutable list
+        mUser.postValue(userFromParse)
+        // switch off error message
+        mIsUserLoadError.postValue(false)
+        // switch off waiting spinner
+        mIsLoading.postValue(false)
+    }
+
+    private fun getUserFromParseServer(username: String) {
+        val query: ParseQuery<ParseUser> = ParseUser.getQuery()
+        query.whereEqualTo("username", username)
+
+        query.findInBackground(FindCallback { objects, e ->
+
+            if (e == null) {
+                if (objects.isNotEmpty()) {
+                    val retrievedUser = objects[0]
+                    retrieveUser(User(retrievedUser.username, "", retrievedUser.email, retrievedUser.get("profileImg").toString()))
+                } else {
+                    mIsLoading.postValue(false)
+                    mIsUserLoadError.postValue(false)
+                    Toast.makeText(getApplication(), "No users here yet", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                // switch off waiting spinner and inform user is smth wrong
+                mIsLoading.postValue(false)
+                // switch off error message
+                mIsUserLoadError.postValue(true)
+                Toast.makeText(getApplication(), e.message, Toast.LENGTH_SHORT).show()
+            }
+        })
+    }
+
 }
