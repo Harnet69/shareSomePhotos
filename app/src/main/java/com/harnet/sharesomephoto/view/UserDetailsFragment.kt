e665e6ae76cda1a880b51e278927a9c0f6480b71
Harnet69 <harnet69@gmail.com>
2020-12-03 08:17:04 +0100

    Implement user images library boilerplate

--- app/src/main/java/com/harnet/sharesomephoto/view/UserDetailsFragment.kt
+++ app/src/main/java/com/harnet/sharesomephoto/view/UserDetailsFragment.kt
@@ -1,14 +1,13 @@
 package com.harnet.sharesomephoto.view
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.harnet.sharesomephoto.R
 import com.harnet.sharesomephoto.databinding.UserDetailsFragmentBinding
 import com.harnet.sharesomephoto.util.setActivityTitle
@@ -23,7 +22,8 @@ class UserDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        dataBinding = DataBindingUtil.inflate(inflater, R.layout.user_details_fragment, container, false)
+        dataBinding =
+            DataBindingUtil.inflate(inflater, R.layout.user_details_fragment, container, false)
 
         return dataBinding.root
     }
@@ -35,17 +35,58 @@ class UserDetailsFragment : Fragment() {
 
         arguments?.let {
             val userId: String = UserDetailsFragmentArgs.fromBundle(it).userId
-           viewModel.refresh(userId)
+            viewModel.refresh(userId)
         }
-
         observeViewModel()
     }
 
     private fun observeViewModel() {
+        userObserve()
+        userImagesObserve()
+    }
+
+    // user observing
+    private fun userObserve(){
         // update the layout using values of mutable variables from a ViewModel
         viewModel.mUser.observe(viewLifecycleOwner, Observer { user ->
             user?.let {
                 dataBinding.user = user
+                user_detail_block_userDeskrFragment.visibility = View.VISIBLE
+            }
+        })
+
+        // make error TextViewVisible
+        viewModel.mIsUserLoadError.observe(viewLifecycleOwner, Observer { isError ->
+            // check isError not null
+            isError?.let {
+                listError_TextView_UserDescr.visibility = if (it) View.VISIBLE else View.GONE
+            }
+        })
+
+        // loading spinner
+        viewModel.mIsLoading.observe(viewLifecycleOwner, Observer { isLoading ->
+            //check isLoading not null
+            isLoading?.let {
+                // if data still loading - show spinner, else - remove it
+                loadingView_ProgressBar_UserDescr.visibility =
+                    if (it) View.VISIBLE else View.GONE
+                if (it) {
+                    //hide all views when progress bar is visible
+                    listError_TextView_UserDescr.visibility = View.GONE
+                    user_detail_block_userDeskrFragment.visibility = View.GONE
+                }
+            }
+        })
+    }
+
+    // user images observe
+    //TODO implement RecyclerView adapter for user images
+    private fun userImagesObserve(){
+        // update the layout using values of mutable variables from a ViewModel
+        viewModel.mUserImages.observe(viewLifecycleOwner, Observer { userImages ->
+            userImages?.let {
+//                dataBinding.user = it
+                // TODO implement adapter of recycler view
                 user_detail_block_userDeskrFragment.visibility = View.VISIBLE
             }
         })
--- app/src/main/java/com/harnet/sharesomephoto/viewModel/UserDetailsViewModel.kt
+++ app/src/main/java/com/harnet/sharesomephoto/viewModel/UserDetailsViewModel.kt
@@ -1,24 +1,32 @@
 package com.harnet.sharesomephoto.viewModel
 
 import android.app.Application
-import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
+import com.harnet.sharesomephoto.model.Image
 import com.harnet.sharesomephoto.model.User
 import com.parse.FindCallback
+import com.parse.ParseObject
 import com.parse.ParseQuery
 import com.parse.ParseUser
 
 class UserDetailsViewModel(application: Application) : BaseViewModel(application) {
+    // user block
     val mUser = MutableLiveData<User>()
     val mIsUserLoadError = MutableLiveData<Boolean>()
     val mIsLoading = MutableLiveData<Boolean>()
 
+    // user images block
+    val mUserImages = MutableLiveData<List<Image>>()
+    val mIsImagesLoadError = MutableLiveData<Boolean>()
+    val mIsImagesLoading = MutableLiveData<Boolean>()
+
     fun refresh(userId: String) {
         getUserById(userId)
+        getUserImages(userId)
     }
 
-    // retrieve images
+    // retrieve a user
     private fun retrieveUser(userFromParse: User) {
         // set received list to observable mutable list
         mUser.postValue(userFromParse)
@@ -28,12 +36,20 @@ class UserDetailsViewModel(application: Application) : BaseViewModel(application
         mIsLoading.postValue(false)
     }
 
+    // retrieve user images
+    private fun retrieveImages(userImagesFromParse: ArrayList<Image>) {
+        // set received list to observable mutable list
+        mUserImages.postValue(userImagesFromParse)
+        // switch off error message
+        mIsImagesLoadError.postValue(false)
+        // switch off waiting spinner
+        mIsImagesLoading.postValue(false)
+    }
+
     private fun getUserById(userId: String) {
         val query: ParseQuery<ParseUser> = ParseUser.getQuery()
         query.whereEqualTo("objectId", userId)
-
         query.findInBackground(FindCallback { objects, e ->
-
             if (e == null) {
                 if (objects.isNotEmpty()) {
                     val retrievedUser = objects[0]
@@ -50,6 +66,42 @@ class UserDetailsViewModel(application: Application) : BaseViewModel(application
                 // switch off error message
                 mIsUserLoadError.postValue(true)
                 Toast.makeText(getApplication(), e.message, Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    // get user images from server
+    private fun getUserImages(authorId: String) {
+        // clean previous version of feeds
+        mUserImages.postValue(arrayListOf())
+
+        val userImages = ArrayList<Image>()
+
+        val query = ParseQuery<ParseObject>("Image")
+        query.whereEqualTo("authorId", authorId)
+        query.orderByDescending("createdAt")
+
+        query.findInBackground(FindCallback { objects, parseObjectError ->
+            if (parseObjectError == null) {
+                if (objects.isNotEmpty()) {
+                    for (image in objects) {
+                        val parseFile = image.getParseFile("image")
+                        val imageForBind = Image(image.get("authorId").toString())
+                        imageForBind.imageURL = parseFile.url
+                        userImages.add(imageForBind)
+                    }
+                    retrieveImages(userImages)
+                } else {
+                    mIsImagesLoadError.postValue(false)
+                    mIsImagesLoading.postValue(false)
+                }
+            } else {
+                // switch off waiting spinner and inform user is smth wrong
+                mIsLoading.postValue(false)
+                // switch off error message
+                mIsImagesLoading.postValue(true)
+                Toast.makeText(getApplication(), parseObjectError.message, Toast.LENGTH_SHORT)
+                    .show()
             }
         })
     }
