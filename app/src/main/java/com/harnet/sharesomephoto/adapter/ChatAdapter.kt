9110ab2c3fd62b6f7fd4802654c729b491089d83
Harnet69 <harnet69@gmail.com>
2021-01-10 10:37:29 +0100

    Add chat adapter

---
+++ app/src/main/java/com/harnet/sharesomephoto/adapter/ChatAdapter.kt
@@ -0,0 +1,36 @@
+package com.harnet.sharesomephoto.adapter
+
+import android.os.Message
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.harnet.sharesomephoto.R
+import com.harnet.sharesomephoto.databinding.ItemChatBinding
+
+class ChatAdapter(private var chatList: ArrayList<com.harnet.sharesomephoto.model.Message>) :
+    RecyclerView.Adapter<ChatAdapter.ChatViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        // elements of the list transforms into views. DataBinding approach
+        val view = DataBindingUtil.inflate<ItemChatBinding>(
+            inflater,
+            R.layout.item_chat,
+            parent,
+            false
+        )
+        return ChatAdapter.ChatViewHolder(view)
+    }
+
+    class ChatViewHolder(var view: ItemChatBinding) : RecyclerView.ViewHolder(view.root)
+
+    override fun getItemCount(): Int {
+        return chatList.size
+    }
+
+    override fun onBindViewHolder(holder: ChatViewHolder, position: Int) {
+        holder.view.msg = chatList[position]
+    }
+}
